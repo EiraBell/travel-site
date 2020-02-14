6a3232b0e71be7acb32be2be367eea24fc308fea
Eira <irinabelova.nz@gmail.com>
2020-02-14 21:07:09 +1300

    Update cloud function to check password.

--- cloud-functions/secret-area.js
+++ cloud-functions/secret-area.js
@@ -1,6 +1,28 @@
 exports.handler = function(event, context, callback) {
-    callback(null, {
-        statusCode: 200,
-        body: "Welcome to the super secret area"
-    })
-}
+    const secretContent = `
+    <h3>Welcom To The Secret Area</h3>
+    <p>Bla-bla-bla</p>
+    `
+
+    let body
+
+    if (event.body) {
+        body = JSON.parse(event.body)
+    } else {
+        body = {}
+    }
+    
+    if (body.password == "javascript") {
+        callback(null, {
+            statusCode: 200,
+            body: secretContent
+        })
+    
+    } else {
+        callback(null, {
+            statusCode: 401
+        })
+    }
+    }
+
+    
